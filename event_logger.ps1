972e5870b5602f8c452490ce9f36e8bb4c3282ce
Jordan Needham <jdncreations@hotmail.com>
2024-12-04 13:08:01 +0800

    http server receives post requests containing event data from client pc

---
+++ event_logger.ps1
@@ -0,0 +1,26 @@
+# check for new events every hour, POST request them to server
+while ($true) {
+  # Check new events in the past X amount of time
+  Write-Host "Running task at: $(Get-Date)"
+  $events = Get-WinEvent -LogName Application | Sort-Object TimeCreated -Descending | Select-Object -First 5
+
+  $events | Format-Table -Property TimeCreated, Id, LevelDisplayName, Message -AutoSize
+
+  $url = "http://localhost:3333/logEvent"
+  $data = $events | ForEach-Object {
+    @{
+      TimeCreated = $_.TimeCreated
+      Id = $_.Id
+      Severity = $_.LevelDisplayName
+      Message = $_.Message
+    }
+  } | ConvertTo-Json -Depth 10
+
+  Write-Host "JSON Payload:"
+  Write-Host $data
+
+  Invoke-RestMethod -Uri $url -Method Post -Headers @{ "Content-Type" = "application/json" } -Body $data
+
+  # Sleep for 1 minutes (60 seconds)
+  Start-Sleep -Seconds 60
+}
--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Event struct {
+	ID int `json:"Id"`
+	Message string `json:"Message"`
+	TimeCreated string `json:"TimeCreated"`
+	Severity string `json:"Severity"`
+}
+
+func handleLog(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Only POST requests are allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if r.Method == "POST" {
+		fmt.Println("POST METHOD RECVD")
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "could not read body", http.StatusInternalServerError)
+			return
+		}
+
+		defer r.Body.Close()
+		var event []Event
+		if err := json.Unmarshal(body, &event); err != nil {
+			http.Error(w, "Failed to parse JSON", http.StatusBadRequest)
+			return
+		}
+
+		// save event to database to be able to use on frontend maybe?
+
+		// Check event ID to see if it's new or not - don't save to DB if it's old
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Event received successfully"))
+	}
+}
 
 func main() {
 	fmt.Println("UP N RUNNIN")
+
+	http.HandleFunc("/logEvent", handleLog)
+
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
 }
