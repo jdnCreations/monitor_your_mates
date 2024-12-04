afbd071308e47e1f2f62ec391a3a71b37df41668
Jordan Needham <jdncreations@hotmail.com>
2024-12-05 07:48:52 +0800

    ran gofmt on main.go

--- main.go
+++ main.go
@@ -18,10 +18,15 @@ import (
 )
 
 type Event struct {
-	ID int `json:"Id"`
-	Message string `json:"Message"`
+	ID          int    `json:"Id"`
+	Message     string `json:"Message"`
 	TimeCreated string `json:"TimeCreated"`
-	Severity string `json:"Severity"`
+	Severity    string `json:"Severity"`
+}
+
+type apiConfig struct {
+	db     *database.Queries
+	secret string
 }
 
 func (cfg *apiConfig) handleLog(w http.ResponseWriter, r *http.Request) {
@@ -45,31 +50,31 @@ func (cfg *apiConfig) handleLog(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-    for _, event := range events {
-      if event.Severity != "Critical" && event.Severity != "Error" && event.Severity != "Warning" {
-        return
-      }
-      time, err := parseDate(event.TimeCreated) 
-      if err != nil {
-        fmt.Println("cannot parse timecreated to time.Time")
-        break
-      }
-      _, err = cfg.db.GetEventById(r.Context(), database.GetEventByIdParams{
-        ID: int32(event.ID),
-        CreatedAt: time,
-      })
-      if err != nil {
-        fmt.Println("event doesn't exist, adding to db.")
-        cfg.db.CreateEvent(r.Context(), database.CreateEventParams{
-          ID: int32(event.ID),
-          Message: sql.NullString{String: event.Message, Valid: true},
-          CreatedAt: time,
-          Severity: sql.NullString{String: event.Severity, Valid: true},
-        })
-      } else {
-        fmt.Println("event already exists")
-      }
-    }
+		for _, event := range events {
+			if event.Severity != "Critical" && event.Severity != "Error" && event.Severity != "Warning" {
+				return
+			}
+			time, err := parseDate(event.TimeCreated)
+			if err != nil {
+				fmt.Println("cannot parse timecreated to time.Time")
+				break
+			}
+			_, err = cfg.db.GetEventById(r.Context(), database.GetEventByIdParams{
+				ID:        int32(event.ID),
+				CreatedAt: time,
+			})
+			if err != nil {
+				fmt.Println("event doesn't exist, adding to db.")
+				cfg.db.CreateEvent(r.Context(), database.CreateEventParams{
+					ID:        int32(event.ID),
+					Message:   sql.NullString{String: event.Message, Valid: true},
+					CreatedAt: time,
+					Severity:  sql.NullString{String: event.Severity, Valid: true},
+				})
+			} else {
+				fmt.Println("event already exists")
+			}
+		}
 
 		// save event to database to be able to use on frontend maybe?
 
@@ -78,11 +83,11 @@ func (cfg *apiConfig) handleLog(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Event received successfully"))
 	}
-  
+
 }
 
 func parseDate(dateStr string) (time.Time, error) {
-  // Use a regular expression to extract the timestamp from the string
+	// Use a regular expression to extract the timestamp from the string
 	re := regexp.MustCompile(`\/Date\((\d+)\)\/`)
 	matches := re.FindStringSubmatch(dateStr)
 
@@ -104,48 +109,43 @@ func parseDate(dateStr string) (time.Time, error) {
 	return time.Unix(seconds, nanoseconds), nil
 }
 
-type apiConfig struct {
-  db *database.Queries
-  secret string
-}
-
 func (cfg *apiConfig) handleEvents(w http.ResponseWriter, r *http.Request) {
-  events, err := cfg.db.GetEvents(r.Context(), 10)
-  if err != nil {
-    http.Error(w, "Unable to fetch events", http.StatusInternalServerError)
-    return
-  }
+	events, err := cfg.db.GetEvents(r.Context(), 10)
+	if err != nil {
+		http.Error(w, "Unable to fetch events", http.StatusInternalServerError)
+		return
+	}
 
-  w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-  err = json.NewEncoder(w).Encode(events)
-  if err != nil {
-    http.Error(w, "Error encoding events to JSON", http.StatusInternalServerError)
-    return
-  }
+	err = json.NewEncoder(w).Encode(events)
+	if err != nil {
+		http.Error(w, "Error encoding events to JSON", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-  godotenv.Load()
-  dbURL := os.Getenv("DB_URL")
-  secret := os.Getenv("SECRET")
-  port := os.Getenv("PORT")
-  db, err := sql.Open("postgres", dbURL)
-  if err != nil {
-    log.Fatal("Could not open connection to db", err)
-  }
-  println("Connected to db")
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	secret := os.Getenv("SECRET")
+	port := os.Getenv("PORT")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Could not open connection to db", err)
+	}
+	println("Connected to db")
 
-  dbQueries := database.New(db)
-  apiConf := apiConfig{}
-  apiConf.db = dbQueries
-  apiConf.secret = secret
+	dbQueries := database.New(db)
+	apiConf := apiConfig{}
+	apiConf.db = dbQueries
+	apiConf.secret = secret
 
 	http.HandleFunc("/logEvent", apiConf.handleLog)
-  http.HandleFunc("/api/events", apiConf.handleEvents)
-  http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/api/events", apiConf.handleEvents)
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
 
-	err = http.ListenAndServe(":" + port, nil)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatalf("Error: %s", err)
 	}
